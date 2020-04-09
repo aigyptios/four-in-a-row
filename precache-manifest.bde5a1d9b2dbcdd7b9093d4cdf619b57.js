92ed0a2470d1df0114545146e97b407eee2306ee
Mike Henry <m.henry511@gmail.com>
2020-04-09 12:16:22 -0400

    Updates

---
+++ precache-manifest.bde5a1d9b2dbcdd7b9093d4cdf619b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea0fb07d95a3f8fecde05ab6711e5fc",
+    "url": "/four-in-a-row/index.html"
+  },
+  {
+    "revision": "f3c74dfa519812ee36df",
+    "url": "/four-in-a-row/static/css/main.9a5ee611.chunk.css"
+  },
+  {
+    "revision": "4658c232bf3988b2b680",
+    "url": "/four-in-a-row/static/js/2.ba9a3998.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/four-in-a-row/static/js/2.ba9a3998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c74dfa519812ee36df",
+    "url": "/four-in-a-row/static/js/main.5db557bc.chunk.js"
+  },
+  {
+    "revision": "39e276daede39ea1f71a",
+    "url": "/four-in-a-row/static/js/runtime-main.691649ed.js"
+  }
+]);
